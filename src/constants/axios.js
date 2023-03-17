5dafed79a75c3be3011d76c261f5bb978687e397
eddymandran <eddy.mandran@gmail.com>
2023-03-17 13:41:06 +0100

    feat(project): add .env for the api url

--- src/constants/axios.js
+++ src/constants/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
+import {API_URL} from "@env"
 
 export default {
     api: axios.create({
-        baseURL: "https://c13b-2a01-cb14-18e-5800-df60-ef6b-7239-bf7e.eu.ngrok.io/api",
+        baseURL: `${API_URL}`
     })
 }
